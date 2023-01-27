33d998c4dbcd6d3f2eae0b2cbdf87c3d71d2dd3b
Henry Wing Fung Yeung <88356994+Henkkk@users.noreply.github.com>
2023-01-27 17:37:09 +0800

    Add files via upload

---
+++ week4/binary_convertor.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+
+    int input_number;
+    string binString = "";
+
+    cout << "Please enter a postive number" << endl;
+    cin >> input_number;
+
+    while (input_number != 0)
+    {
+        if (input_number%2 == 0)
+        {
+            binString = "0" + binString;
+        }
+        else
+        {
+            binString = "1" + binString;
+        }
+
+        input_number = input_number / 2;
+    }
+    
+    cout << "Binary Form = " << binString << endl;
+
+    return 0;
+}
+ 
